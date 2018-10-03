2c32ba29d99bb072cbfbeee791960bda8bdf4ef8
Justin Schneck <jschneck@mac.com>
2018-10-03 14:27:16 -0400

    nerves_system_br: bump to v1.5.2

--- mix.exs
+++ mix.exs
@@ -59,8 +59,8 @@ defmodule KioskSystemx8664.Mixfile do
   defp deps do
     [
       {:nerves, "~> 1.0", runtime: false},
-      {:nerves_system_br, "~> 1.4.1", runtime: false},
-      {:nerves_toolchain_x86_64_unknown_linux_gnu , "~> 1.1.0", runtime: false},
+      {:nerves_system_br, "1.5.2", runtime: false},
+      {:nerves_toolchain_x86_64_unknown_linux_gnu , "1.1.0", runtime: false},
       {:nerves_system_linter, "~> 0.3.0", runtime: false},
       {:ex_doc, "~> 0.18", only: :dev}
     ]
